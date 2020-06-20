c8cfc97941f48481ba6dd351e2709362744a70bd
unknown <Family@DRORZ-LT3.il.corp.radware.com>
2020-06-20 22:01:08 +0300

    add todo-item

---
+++ src/components/todo-item/index.tsx
@@ -0,0 +1,23 @@
+import React from "react";
+import { Todo } from "../../store/todos/type";
+import { reviveTodo } from "../../store/todos/slice";
+import { useDispatch } from "react-redux";
+
+interface TodoItemProps {
+  todo: Todo;
+}
+
+export function TodoItem({
+  todo: { checked, content, removed, id },
+}: TodoItemProps) {
+  const dispatch = useDispatch();
+  return (
+    <>
+      {!removed && <input type="checkbox" value={checked.toString()} />}
+      <span>{content}</span>
+      {removed && (
+        <button onClick={() => dispatch(reviveTodo(id))}>revive</button>
+      )}
+    </>
+  );
+}
